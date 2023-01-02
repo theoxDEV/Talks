5732cdbf4c2f5828fb7a358ecdd70ee4a90a4974
MATHEUS NASCIMENTO OLIVEIRA <theusnascimento1999@outlook.com>
2023-01-02 19:34:05 -0300

    Adicionar arquivos de projeto.

---
+++ Talks/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Talks;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Talks/Pages/LanguageSelect/LanguageSelect.xaml.cs
@@ -0,0 +1,9 @@
+namespace Talks.Pages.LanguageSelect;
+
+public partial class LanguageSelect : ContentPage
+{
+	public LanguageSelect()
+	{
+		InitializeComponent();
+	}
+}
